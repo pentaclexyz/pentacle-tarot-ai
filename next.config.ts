a14f47e9632ff653c06fe6d450f953be98695e30
pentcle <pentacle@pentacle.xyz>
2025-02-12 19:06:46 +0000

    lodash

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+// next.config.js
+const path = require('path');
 
-const nextConfig: NextConfig = {
-  /* config options here */
+module.exports = {
+    webpack: (config) => {
+        config.resolve.alias['lodash/compact'] = path.resolve(__dirname, 'node_modules/lodash/compact.js');
+        config.resolve.alias['lodash/at'] = path.resolve(__dirname, 'node_modules/lodash/at.js');
+        config.resolve.alias['lodash/clone'] = path.resolve(__dirname, 'node_modules/lodash/clone.js');
+        config.resolve.alias['lodash/extend'] = path.resolve(__dirname, 'node_modules/lodash/extend.js');
+        config.resolve.alias['lodash/filter'] = path.resolve(__dirname, 'node_modules/lodash/filter.js');
+        return config;
+    },
 };
-
-export default nextConfig;
