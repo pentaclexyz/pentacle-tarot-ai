d974a85e421a1754f0df6c61677c6dfb0baf4493
pentcle <pentacle@pentacle.xyz>
2025-02-12 19:14:03 +0000

    lodash weird stuff

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,25 @@
-// next.config.js
-const path = require('path');
+import path from 'path';
+import { Configuration } from 'webpack';
 
-module.exports = {
-    webpack: (config) => {
-        config.resolve.alias['lodash/compact'] = path.resolve(__dirname, 'node_modules/lodash/compact.js');
-        config.resolve.alias['lodash/at'] = path.resolve(__dirname, 'node_modules/lodash/at.js');
-        config.resolve.alias['lodash/clone'] = path.resolve(__dirname, 'node_modules/lodash/clone.js');
-        config.resolve.alias['lodash/extend'] = path.resolve(__dirname, 'node_modules/lodash/extend.js');
-        config.resolve.alias['lodash/filter'] = path.resolve(__dirname, 'node_modules/lodash/filter.js');
+const nextConfig = {
+    webpack: (config: Configuration) => {
+        // Ensure config.resolve exists
+        config.resolve = config.resolve || {};
+
+        // Cast alias to a record so we can index it with custom keys
+        const alias = config.resolve.alias as { [key: string]: string } || {};
+
+        alias['lodash/compact'] = path.resolve(__dirname, 'node_modules/lodash/compact.js');
+        alias['lodash/at'] = path.resolve(__dirname, 'node_modules/lodash/at.js');
+        alias['lodash/clone'] = path.resolve(__dirname, 'node_modules/lodash/clone.js');
+        alias['lodash/extend'] = path.resolve(__dirname, 'node_modules/lodash/extend.js');
+        alias['lodash/filter'] = path.resolve(__dirname, 'node_modules/lodash/filter.js');
+
+        // Assign the updated alias back to config.resolve.alias
+        config.resolve.alias = alias;
+
         return config;
     },
 };
+
+export default nextConfig;
